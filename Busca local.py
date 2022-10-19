b8fc1c39f66a846ca0cb821a2dc66a9bfd904753
Ricarte Jr <ricarte.junior@gmail.com>
2022-10-18 21:25:39 -0300

    Teste

---
+++ Busca local.py
@@ -0,0 +1,32 @@
+# *args(a, 'a') retorna uma tupla (value)
+# **kwargs(e='eeee') retorna um dicionario {'key': 'value'}
+from fun.func import copycopy
+from os import path, walk
+
+
+contador = 0
+des_local = r'D:/VIDEOS/Videos YT/1 - Python Downloads/BuscasPython'
+
+print('\n\nOpciones: Buscar por nombre o extension.\n\n')
+buscar_en = path.abspath(input('Buscar en  -> ').strip())
+buscar_por = str(input('Buscar por -> ').lower().strip())
+
+if path.exists(buscar_en) == True:
+    for camino, carpetas, archivos in walk(f'{buscar_en}', topdown=True, onerror=None):
+        for archivo in archivos:
+            if archivo.lower().find(buscar_por) != -1:
+                contador += 1
+                try:
+                    des_arq = path.abspath(f'{des_local}/{buscar_por}')
+                    copy_arq = path.abspath(f'{camino}/{archivo}')
+                    # copycopy(copy_arq, des_arq, des_local, buscar_por, archivo)
+                except KeyboardInterrupt as error:
+                    print('Cancelado por el usuario.')
+                    break
+                else:
+                    print(f'{"":-<160}')
+                    print('Copiado ->', path.abspath(f'{camino}/{archivo}'))
+else:
+    print(f'\n-> {buscar_en} Invalido.\n')
+
+print(f'\n->[{contador}] - [{buscar_por}] encontrados.\n')
